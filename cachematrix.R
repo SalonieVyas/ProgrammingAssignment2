2bcb1a7ec3e7c0b5a04ad47d47dc6567becc8c36
SalonieVyas <salonie.mockingbird@gmail.com>
2015-01-25 00:14:19 +0530

    Programming Assignment 2 Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The assignment is regarding lexical scoping and caching functions that may require a long computation time. 
 
-## Write a short comment describing this function
+
+
+## The first function, makeCacheMatrix creates a Matrix, which is really a list containing a function to
+##set the value of the matrix
+##get the value of the matrix
+
+##
+
+## The following function takes an i/p as a matrix and sets it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+##the function cacheSolve produces the inverse of the matrix given as an input
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+        
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
